5ba776036b48269b997ab5b57fcb1ab5497cf5ed
Eliott Dumont <eliott@MacBook-Pro.local>
2023-09-05 16:26:53 +0200

    opti

--- opti.py
+++ opti.py
@@ -14,7 +14,7 @@ ema_window = 1'''
 
 stock = "BTC-USD"
 start_date = "2010-01-01"
-end_date = "2023-02-01"
+end_date = "2023-04-01"
 frais = 99.5/100
 
 btc_data = yf.download(stock, start=start_date, end=end_date, interval="1d")
@@ -29,12 +29,15 @@ def strat(stock, start_date, end_date, PHI, PHI2, ema_window, frais):
     btc_data['LogPrice'] = np.log(btc_data['Close'])
     lreturns = btc_data['LogReturns']
     close = btc_data['LogPrice']
-    btc_data['EMA_LogReturns'] = btc_data['LogReturns'].ewm(span=ema_window).mean()
+    #btc_data['EMA_LogReturns'] = btc_data['LogReturns'].ewma(span=ema_window).mean()
+    btc_data['EMA_LogReturns'] = pd.Series.ewm(btc_data['LogReturns'], span=ema_window).mean()
     #btc_data['EMA_LogReturns'] = btc_data['LogReturns']
 
 
 
     PNL = [0]
+    long = [0]
+    short = [0]
     trend = 0
     openpos = btc_data['Close'][0]
     i = 0
@@ -43,32 +46,44 @@ def strat(stock, start_date, end_date, PHI, PHI2, ema_window, frais):
         if trend == 1 and used == 0:
             if signal > -PHI2:
                 PNL.append(PNL[-1])
+                long.append(0)
+                short.append(0)
             if signal <= -PHI:
                 trend = -1
                 used = 1
                 closepos = btc_data['Close'][i]
                 PNL.append(PNL[-1] + frais*(closepos - openpos))
+                long.append(-1)
+                short.append(1)
                 openpos = closepos
             if signal <= -PHI2 and signal > -PHI:
                 trend = 0
                 used = 1
                 closepos = btc_data['Close'][i]
                 PNL.append(PNL[-1] + frais*(closepos - openpos))
+                long.append(-1)
+                short.append(0)
                 
         if trend == -1 and used ==0:
             if signal < PHI2:
                 PNL.append(PNL[-1])
+                long.append(0)
+                short.append(0)
             if signal >= PHI:
                 trend = 1
                 used = 1
                 closepos = btc_data['Close'][i]
                 PNL.append(PNL[-1] + frais*(openpos - closepos))
+                long.append(1)
+                short.append(-1)
                 openpos = closepos
             if signal >= PHI2 and signal < PHI:
                 trend = 0
                 used = 1
                 closepos = btc_data['Close'][i]
                 PNL.append(PNL[-1] + frais*(openpos - closepos))
+                long.append(0)
+                short.append(-1)
                 
         if trend == 0 and used ==0:
             if signal > PHI:
@@ -76,17 +91,25 @@ def strat(stock, start_date, end_date, PHI, PHI2, ema_window, frais):
                 used = 1
                 openpos = btc_data['Close'][i]
                 PNL.append(PNL[-1])
+                long.append(1)
+                short.append(0)
                 
             elif signal < -PHI:
                 trend = -1
                 used = 1
                 openpos = btc_data['Close'][i]
                 PNL.append(PNL[-1])
+                long.append(0)
+                short.append(1)
                 
             else: 
                 PNL.append(PNL[-1])
+                long.append(0)
+                short.append(0)
         i+=1
     PNL.pop(0)
+    long.pop(0)
+    short.pop(0)
 
     maxloss = np.min(PNL)
     vol = np.std(PNL)
@@ -97,7 +120,7 @@ def strat(stock, start_date, end_date, PHI, PHI2, ema_window, frais):
     
     # Create a grid of subplots
     fig = plt.figure(figsize=(12, 7))
-    gs = fig.add_gridspec(3, 1, hspace=0.2)
+    gs = fig.add_gridspec(5, 1, hspace=0.2)
 
     # Subplot 1: Plot BTC-USD prices
     ax1 = fig.add_subplot(gs[0, 0])
@@ -121,6 +144,19 @@ def strat(stock, start_date, end_date, PHI, PHI2, ema_window, frais):
     ax3.set_ylabel('P&L')
     ax3.legend()
     ax3.grid(True)
+
+    
+    ax4 = fig.add_subplot(gs[3, 0], sharex=ax1)
+    ax4.plot(btc_data.index, long, label='Actions', color='black')
+    ax4.set_ylabel('Long')
+    ax4.legend()
+    ax4.grid(True)
+
+    ax5 = fig.add_subplot(gs[4, 0], sharex=ax1)
+    ax5.plot(btc_data.index, short, label='Actions', color='black')
+    ax5.set_ylabel('Short')
+    ax5.legend()
+    ax5.grid(True)
 
     # Set the title for the entire figure
     plt.suptitle('Stock Price, EMA of Log Returns, and P&L (daily)')
